d53d0870bb51f0c20f6dfeb3266aab52bd32d521
hveficent <hector.villarreal@eficent.com>
2019-07-30 18:13:29 +0200

    [MIG] project_task_report: Migration to 12.0

--- project_task_report/__manifest__.py
+++ project_task_report/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -6,7 +5,7 @@
 {
     "name": "Project Task Report",
     "summary": "Basic report for project tasks.",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, "
             "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project-reporting",
