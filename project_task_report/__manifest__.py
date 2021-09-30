206a7475f1e630d9d5cced91cf04fd64ab6f3435
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-09-29 19:13:23 -0500

    [MIG] project_task_report: Migration to V15

--- project_task_report/__manifest__.py
+++ project_task_report/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Project Task Report",
     "summary": "Basic report for project tasks.",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Eficent, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project-reporting",
     "category": "Project Management",
