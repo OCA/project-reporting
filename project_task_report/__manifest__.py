32b89c6c6989222816beb3283b11206d20ee3347
Borruso <gborruso@dinamicheaziendali.it>
2024-12-20 15:23:07 +0100

    [MIG] project_task_report: Migration to 18.0

--- project_task_report/__manifest__.py
+++ project_task_report/__manifest__.py
@@ -5,12 +5,16 @@
 {
     "name": "Project Task Report",
     "summary": "Basic report for project tasks.",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project-reporting",
     "category": "Project Management",
     "depends": ["project"],
-    "data": ["views/project_task_report.xml", "views/project_task_chatter_report.xml"],
+    "data": [
+        "views/project_task_document.xml",
+        "views/project_task_report.xml",
+        "views/project_task_chatter_report.xml",
+    ],
     "license": "AGPL-3",
     "installable": True,
     "application": False,
