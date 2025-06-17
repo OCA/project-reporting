72a73f340526b9165c7af0ea77ea19f2488ca592
Adasat Torres de León <a.torres@binhex.cloud>
2025-06-17 11:01:21 +0100

    [MIG] project_task_report: Migration to 17.0

--- project_task_report/__manifest__.py
+++ project_task_report/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Project Task Report",
     "summary": "Basic report for project tasks.",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project-reporting",
     "category": "Project Management",
